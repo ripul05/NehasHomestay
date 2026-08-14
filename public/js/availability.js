f6cad711b3dc51593b8838c3e9a2bd29192d786e
ripul <ripul@ripuls-MacBook-Air.local>
2026-08-14 23:48:08 +0900

    feat: corrected routes

--- public/js/availability.js
+++ public/js/availability.js
@@ -40,8 +40,8 @@ document.addEventListener("DOMContentLoaded", () => {
      */
 
     const API_BASE_URL =
-        // "https://nehas-homestay.vercel.app";
-        "http://localhost:3000";
+        "https://nehas-homestay.vercel.app";
+        // "http://localhost:3000";
 
 
     /*
